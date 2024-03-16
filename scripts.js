d7594ca68a8baac5cd5c4e36faac905826c62a2b
mmnw <hello@mmnw.dev>
2024-03-16 11:43:26 +0300

    Update 2024-03-16 11:43:26

--- scripts.js
+++ scripts.js
@@ -10,6 +10,18 @@ $(document).ready(function () {
 
 
 
+$(document).ready(function() {
+    $('.modal-object-button').on('click', function() {
+        // Находим ближайший элемент с классом modal-object-heading
+        var heading = $(this).closest('.modal').find('.modal-object-heading').text();
+        
+        // Записываем текст заголовка в скрытое поле формы
+        $('.wait-form #object-input').val(heading);
+    });
+});
+
+
+
 // Слайдер контакты
 document.addEventListener('DOMContentLoaded', function () {
     var clients_slider = new Swiper('#contacts-slider', {
